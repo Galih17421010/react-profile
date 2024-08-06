49583d09a9964f83de458bc30a8d0f40ab4a602d
Galih Agus Saputra <agussaputragalih@gmail.com>
2024-08-07 00:39:52 +0700

    Example Pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,10 @@ import { About } from "./components/pages/About";
 import { Projects } from "./components/pages/Project";
 import { Experiences } from "./components/pages/Experience";
 import { Articles } from "./components/pages/Article";
-import { Cta } from "./components/Cta";
-import { FAQ } from "./components/FAQ";
-import { Features } from "./components/Features";
 import { Footer } from "./components/elements/Footer";
-import { Hero } from "./components/Hero";
-import { HowItWorks } from "./components/HowItWorks";
 import { Navbar } from "./components/elements/Navbar";
-import { Newsletter } from "./components/Newsletter";
-import { Pricing } from "./components/Pricing";
 import { ScrollToTop } from "./components/elements/ScrollToTop";
-import { Services } from "./components/Services";
-import { Sponsors } from "./components/Sponsors";
-import { Team } from "./components/Team";
-import { Testimonials } from "./components/Testimonials";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import "./App.css";
 
 function App() {
@@ -34,18 +22,6 @@ function App() {
           <Route path="/articles" element={<Articles />} />
         </Routes>
       </Router>
-      {/* <Hero />
-      <Sponsors />
-      <About />
-      <HowItWorks />
-      <Features />
-      <Services />
-      <Cta />
-      <Testimonials />
-      <Team />
-      <Pricing />
-      <Newsletter />
-      <FAQ /> */}
       <Footer />
       <ScrollToTop />
     </>
