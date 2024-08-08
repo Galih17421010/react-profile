d4266448f150f85d9cae3e979341d6503a4fc1df
Galih Agus Saputra <agussaputragalih@gmail.com>
2024-08-08 13:03:32 +0700

    Project Page Card & Pagination

--- src/components/pages/Project.tsx
+++ src/components/pages/Project.tsx
@@ -3,26 +3,58 @@ import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/componen
 import image from "@/assets/growth.png";
 import image3 from "@/assets/reflecting.png";
 import image4 from "@/assets/looking-ahead.png";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { buttonVariants } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+import { Pagination, PaginationContent, PaginationItem, PaginationNext, PaginationPrevious } from "../ui/pagination";
 
 interface ProjectProps {
   title: string;
+  slug: string;
   description: string;
   image: string;
 }
 
 const projects: ProjectProps[] = [
   {
-    title: "Responsive Design",
+    title: "Project 1",
+    slug: "project-1",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nesciunt est nostrum omnis ab sapiente.",
     image: image4,
   },
   {
-    title: "Intuitive user interface",
+    title: "Project 2",
+    slug: "project-2",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nesciunt est nostrum omnis ab sapiente.",
     image: image3,
   },
   {
-    title: "AI-Powered insights",
+    title: "Project 3",
+    slug: "project-3",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nesciunt est nostrum omnis ab sapiente.",
+    image: image,
+  },
+  {
+    title: "Project 4",
+    slug: "project-4",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nesciunt est nostrum omnis ab sapiente.",
+    image: image,
+  },
+  {
+    title: "Project 5",
+    slug: "project-5",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nesciunt est nostrum omnis ab sapiente.",
+    image: image,
+  },
+  {
+    title: "Project 6",
+    slug: "project-6",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nesciunt est nostrum omnis ab sapiente.",
+    image: image,
+  },
+  {
+    title: "Project 7",
+    slug: "project-7",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi nesciunt est nostrum omnis ab sapiente.",
     image: image,
   },
@@ -34,7 +66,7 @@ export const Projects = () => {
   return (
     <section id="projects" className="container py-24 sm:py-10 space-y-8">
       <h2 className="text-3xl lg:text-4xl font-bold md:text-center">
-        Many <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">Great Projects</span>
+        Many <span className="text-primary">Great Projects</span>
       </h2>
 
       <div className="flex flex-wrap md:justify-center gap-4">
@@ -48,20 +80,50 @@ export const Projects = () => {
       </div>
 
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {projects.map(({ title, description, image }: ProjectProps) => (
+        {projects.map(({ title, slug, description, image }: ProjectProps) => (
           <Card key={title}>
             <CardHeader>
-              <CardTitle>{title}</CardTitle>
+              <Link to={slug} className="text-primary">
+                <img src={image} alt="About feature" className="w-[100px] lg:w-[200px] mx-auto" />
+                <CardTitle>{title}</CardTitle>
+              </Link>
             </CardHeader>
 
             <CardContent>{description}</CardContent>
 
-            <CardFooter>
-              <img src={image} alt="About feature" className="w-[200px] lg:w-[300px] mx-auto" />
+            <CardFooter className="justify-center">
+              <div>
+                <a
+                  rel="noreferrer noopener"
+                  href="https://github.com/Galih17421010"
+                  target="_blank"
+                  className={buttonVariants({
+                    variant: "ghost",
+                    size: "sm",
+                  })}
+                >
+                  <span className="sr-only">Github icon</span>
+                  <GitHubLogoIcon className="w-5 h-5" />
+                </a>
+              </div>
             </CardFooter>
           </Card>
         ))}
       </div>
+      <div className="flex flex-wrap md:justify-center gap-4">
+        <Pagination>
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious href="#" />
+            </PaginationItem>
+
+            <PaginationItem>
+              <PaginationNext href="#" />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      </div>
+      <div className="flex flex-wrap md:justify-center gap-4"></div>
     </section>
   );
 };
